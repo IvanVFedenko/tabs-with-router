32dcc6a0e6185fb98a95c184d883743291075b19
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-07-31 14:00:30 +0300

    Updates

---
+++ precache-manifest.3aeb7b27ef5f3ea87532f2bbd6af5da1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c477b36e6646ae37ad638913d4cdc7d",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "37df76e5fbd421f8a356",
+    "url": "/react_tabs-with-router/static/css/main.ead98f10.chunk.css"
+  },
+  {
+    "revision": "15c1bddc8585e12fa088",
+    "url": "/react_tabs-with-router/static/js/2.d43ea8ad.chunk.js"
+  },
+  {
+    "revision": "37df76e5fbd421f8a356",
+    "url": "/react_tabs-with-router/static/js/main.31a45978.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
